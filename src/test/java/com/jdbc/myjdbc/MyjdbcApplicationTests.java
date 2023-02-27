cf3ece05589c17df56dde3e6ec100c020461564a
sanix <sanix@thinksan.mshome>
2023-02-27 22:54:05 +0400

    Initial commit

---
+++ src/test/java/com/jdbc/myjdbc/MyjdbcApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jdbc.myjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert true;
+	}
+
+}
